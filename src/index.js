89222e1e2743f8ea53ff3f346493809e1b18e6ef
mitran <mitran@example>
2021-07-20 21:13:34 +0300

    react & jsx (no need for react import)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-import "./styles/index.scss";
+import { render } from "react-dom";
+import App from "./components/App";
 
-const elvenShieldRecipe = {
-  letherStrips: 2,
-  ironIngot: 1,
-  refinedMoonstone: 4,
-};
-
-const elvenGauntletsRecipe = {
-  ...elvenShieldRecipe,
-  leather: 1,
-  refinedMoonstone: 2,
-};
-
-console.log(elvenShieldRecipe);
-console.log(elvenGauntletsRecipe);
+render(<App />, document.getElementById("root"));
